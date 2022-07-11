a9d8159f7fc7b3d5d441acb1b8ba074f7612982c
renanbarhbosa <renan-dealmeida@hotmail.com>
2022-07-11 14:17:21 -0300

    First component

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,12 @@
+import icon from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
 function NotificationButton() {
     return (
-        <h1>Olá, mundo!</h1>
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt='Notificar' />
+        </div>
     )
 }
 
