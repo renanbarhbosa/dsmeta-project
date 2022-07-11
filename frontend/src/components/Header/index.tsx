cd5745195d69eb08c970b29b58a1d23ac364b431
renanbarhbosa <renan-dealmeida@hotmail.com>
2022-07-11 14:46:24 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Logo from '../../assets/img/Logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <div className="dsmeta-logo-container">
+            <img src={Logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por Renan Barbosa:
+                    <a href="https://github.com/renanbarhbosa">Github</a>
+                </p>
+        </div>
+    )
+}
+
+export default Header
