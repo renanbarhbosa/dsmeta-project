7e8cd53c11164e3f0104948fa3c2228a65f01103
renanbarhbosa <renan-dealmeida@hotmail.com>
2022-07-13 17:24:13 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmeta.repositories;
+
+import com.devsuperior.dsmeta.config.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+
+}
